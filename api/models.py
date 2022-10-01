fb2527b7fe23bba7bbcae28ac49fe8a96668f6d2
PhantomShow <gonzalezjohn9983@yahoo.com>
2022-10-01 18:18:56 -0400

    Update models.py

--- api/models.py
+++ api/models.py
@@ -1,6 +1,7 @@
 from django.contrib import auth
 
 # Create your models here.
+# Need to fix regular user login and add both athlete and scout model
 class User(auth.models.User, auth.models.PermissionsMixin):
 
     def __str__(self):
