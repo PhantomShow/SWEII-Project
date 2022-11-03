68826d245ce562befbe14e0acbc387fd1fd07b3f
ujuar001 <ujuar001@fiu.edu>
2022-11-03 12:57:44 -0400

    updated index page & profile page

--- swe_project/settings.py
+++ swe_project/settings.py
@@ -122,7 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIR = BASE_DIR / 'static'
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
