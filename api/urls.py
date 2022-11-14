02daeb38700bdc1633ce91a4d3ac9d3389ede547
PhantomShow <gonzalezjohn9983@yahoo.com>
2022-11-13 20:10:16 -0500

    Reworked Home View
    Reworked home view into a function view to work with form and list. Did not work with class based view.

--- api/urls.py
+++ api/urls.py
@@ -7,7 +7,7 @@ from . import views
 app_name = 'api'
 
 urlpatterns = [
-    path('', views.HomePage.as_view(), name='home_page'),
+    path('', views.home_page, name='home_page'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login_page'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout_page'),
     path('signup/', views.SignUp.as_view(), name='signup_page'),
--- api/views.py
+++ api/views.py
@@ -2,23 +2,40 @@ from django.shortcuts import render
 from django.views.generic import TemplateView, CreateView, ListView, DetailView, UpdateView
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
+from django.contrib.auth import get_user_model
 from . import forms
 from . import models
 # Create your views here.
 
-class HomePage(ListView):
-    model = models.CustomUser
-    template_name = 'index.html'
-    context_object_name = 'user_list'
+# class HomePage(ListView):
+#     model = models.CustomUser
+#     template_name = 'index.html'
+#     context_object_name = 'user_list'
 
-    # This makes it possible to have both a user_list, posts list, and form in one view
-    def get_context_data(self, **kwargs):
-        context = super(HomePage, self).get_context_data(**kwargs)
-        context.update({
-            'posts': models.Post.objects.all().order_by('-create_date'), # Newest first
-            'form': forms.PostForm,
-        })
-        return context
+#     # This makes it possible to have both a user_list, posts list, and form in one view
+#     def get_context_data(self, **kwargs):
+#         context = super(HomePage, self).get_context_data(**kwargs)
+#         context.update({
+#             'posts': models.Post.objects.all().order_by('-create_date'), # Newest first
+#             'form': forms.PostForm,
+#         })
+#         return context
+
+def home_page(request):
+    user_list = models.CustomUser.objects.all()
+    posts = models.Post.objects.all()
+
+    if request.method == 'POST':
+        form = forms.PostForm(request.POST)
+        if form.is_valid():
+            form.instance.author = request.user
+            form.save()
+    else:
+        form = forms.PostForm()
+    
+    context = {'user_list': user_list, 'posts':posts, 'form':form}
+
+    return render(request, 'index.html', context)
 
 class UserDetail(DetailView):
     model = models.CustomUser
